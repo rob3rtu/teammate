e5564e6585eec11cfafc557e85f61c4b5a53d6d3
Robert <robert.udrea@cst.ro>
2025-07-29 15:36:02 +0300

    move reset setup in settings

--- src/app/(pages)/(settings)/index.tsx
+++ src/app/(pages)/(settings)/index.tsx
@@ -1,11 +1,15 @@
+import { AuthContext } from "@/app/_layout";
 import PageView from "@/layouts/PageView";
 import { supabase } from "@/utils/supabase";
 import { useMutation } from "@tanstack/react-query";
+import { useContext } from "react";
 import { Alert } from "react-native";
 import { Button, Text, useTheme } from "react-native-paper";
 
 export default function SettingsPage() {
   const theme = useTheme();
+  const { authenticatedAccount } = useContext(AuthContext);
+
   const logOutMutation = useMutation({
     mutationFn: async () => {
       await supabase.auth.signOut();
@@ -13,11 +17,32 @@ export default function SettingsPage() {
     onError: (error) => Alert.alert(error.message),
   });
 
+  const handleResetSetup = async () => {
+    const { error } = await supabase
+      .from("profiles")
+      .update({ setup: false })
+      .eq("id", authenticatedAccount?.id);
+
+    if (error) {
+      Alert.alert(error.message);
+    }
+    supabase.auth.refreshSession();
+  };
+
   return (
-    <PageView style={{ gap: 24 }}>
+    <PageView style={{ gap: 16, alignItems: "center", width: "100%" }}>
       <Text>Hello settings page</Text>
 
       <Button
+        style={{ width: "100%" }}
+        mode="outlined"
+        onPress={handleResetSetup}
+      >
+        Reset setup
+      </Button>
+
+      <Button
+        style={{ width: "100%" }}
         mode="contained"
         onPress={() => logOutMutation.mutateAsync()}
         loading={logOutMutation.isPending}
--- src/app/(pages)/(timeline)/index.tsx
+++ src/app/(pages)/(timeline)/index.tsx
@@ -1,24 +1,10 @@
 import { AuthContext } from "@/app/_layout";
 import PageView from "@/layouts/PageView";
-import { supabase } from "@/utils/supabase";
 import { useContext } from "react";
-import { Alert } from "react-native";
-import { Button, Surface, Text } from "react-native-paper";
+import { Surface, Text } from "react-native-paper";
 
 export default function HomeScreen() {
   const { authenticatedAccount } = useContext(AuthContext);
-
-  const handleResetSetup = async () => {
-    const { error } = await supabase
-      .from("profiles")
-      .update({ setup: false })
-      .eq("id", authenticatedAccount?.id);
-
-    if (error) {
-      Alert.alert(error.message);
-    }
-    supabase.auth.refreshSession();
-  };
 
   return (
     <PageView style={{ alignItems: "flex-start" }}>
@@ -29,10 +15,6 @@ export default function HomeScreen() {
       <Surface style={{ padding: 10 }}>
         <Text>Hello surface</Text>
       </Surface>
-
-      <Button mode="outlined" onPress={handleResetSetup}>
-        Reset setup
-      </Button>
     </PageView>
   );
 }
