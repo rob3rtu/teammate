17b7a371da86e9aa4718380375639f3c64a00760
Robert <robert.udrea@cst.ro>
2025-08-01 22:19:36 +0300

    baby steps setup MapView

--- src/app/(pages)/(courts)/index.tsx
+++ src/app/(pages)/(courts)/index.tsx
@@ -1,10 +1,13 @@
 import PageView from "@/layouts/PageView";
-import { Text } from "react-native-paper";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 
 export default function Courts() {
   return (
-    <PageView>
-      <Text>Courts page</Text>
+    <PageView style={{ padding: 0 }}>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={{ width: "100%", height: "100%" }}
+      />
     </PageView>
   );
 }
