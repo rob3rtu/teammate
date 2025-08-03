9661c3bc2892c2882ce1f03eccbc4789398e80a0
Robert <robert.udrea@cst.ro>
2025-08-03 09:16:22 +0300

    going with default apple maps

--- src/app/(pages)/(courts)/index.tsx
+++ src/app/(pages)/(courts)/index.tsx
@@ -1,13 +1,10 @@
 import PageView from "@/layouts/PageView";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import MapView from "react-native-maps";
 
 export default function Courts() {
   return (
     <PageView style={{ padding: 0 }}>
-      <MapView
-        provider={PROVIDER_GOOGLE}
-        style={{ width: "100%", height: "100%" }}
-      />
+      <MapView style={{ width: "100%", height: "100%" }} />
     </PageView>
   );
 }
